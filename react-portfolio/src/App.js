eed87d0553b1ddee2e975c2166d866506fc4a5d6
ianjustinferris <ianjustinferris@gmail.com>
2022-07-25 20:20:53 -0400

    FIxed about responsiveness

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='app'>
       <PortfolioContainer />
-      <Footer fixed="bottom" />
+      <Footer />
     </div>
 
   );
--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import portrait from '../../assets/images/headshot.png'
+import portrait from '../../assets/images/headshotBW.png'
 
 export default function About() {
-    return
+    return (
+        <div className="About">
+            <h1 className='aboutHeader'>ABOUT</h1>
+            <img className='portrait animate__animated animate__fadeInUpBig' src={portrait} alt="portrait"></img>
+            <h2 className='aboutText'> A background in Engineering and Art, I'm looking to integrate those skills into the world of Web-Development. Most recently I've been
+                interested in Machine Learning applications as they relate to how we interact with the world on a daily basis.</h2>
+        </div>
+    )
 }
